7e7eabd688a92b7c852e32b59b535aba1645d6ea
patrick <leepc777@live.com>
2019-03-19 03:11:31 -0700

    completed three calculation buttons : AC , +/- , %

--- SimpleCaculator/ViewController.swift
+++ SimpleCaculator/ViewController.swift
@@ -10,12 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    //make isFinishedTypingNumber private so it can only be accessed inside this class
+    //makes global variable isFinishedTypingNumber private so it can only be accessed inside this class
     // a tracker to keep trakcing typing number has been done
     // when typing is down, display the new single bit number
     // when typing is still going, cancaternate the single bit number to previous number.
     private var isFinishedTypingNumber: Bool = true
     
+    private var displayValue: Double {
+        get {
+            guard let value = Double(displayLabel.text!) else {
+                fatalError("can't convert displayLabel.text to a Double")
+            }
+            return value
+        }
+    }
     
     var displayNumber : String = ""
 
@@ -24,6 +32,23 @@ class ViewController: UIViewController {
     
     @IBAction func calcButtonTapped(_ sender: UIButton) {
         isFinishedTypingNumber = true
+        
+        guard let calMethod = sender.currentTitle else {
+            fatalError(" calButtom.currentTitle as nil")
+        }
+        
+//        if let calMethod = sender.currentTitle
+        switch calMethod {
+        case "+/-":
+            displayLabel.text = "\(displayValue * -1)"
+        case "AC":
+            displayLabel.text = "0"
+        case "%":
+            displayLabel.text = "\(displayValue * 0.01)"
+        default:
+            ()
+        }
+        
     }
     
     @IBAction func numButtonTapped(_ sender: UIButton) {
@@ -39,8 +64,19 @@ class ViewController: UIViewController {
             isFinishedTypingNumber = false
         } else {
             // still typing the new number.
-//            displayNumber += number
-//            displayLabel.text = displayNumber
+            
+            // use floor to round double(remove decimal point).
+            // the ideal is you can only add . to Int. But this is too complicated. No good
+//            if number == "." {
+//                let isInt = ( floor(displayValue) == displayValue )
+//                if !isInt {return}
+//            }
+            
+            if number == "." {
+//            guard !displayLabel.text!.contains(".") else {return}
+                if displayLabel.text!.contains(".") {return}
+            }
+            
             displayLabel.text = displayLabel.text! + number
         }
         
