82a9e72013a9cbc5b0fd74d37df0a2bd43d088c1
patrick <leepc777@live.com>
2019-03-19 01:58:13 -0700

    done accumulate numbers. no calculation yet.

--- SimpleCaculator/ViewController.swift
+++ SimpleCaculator/ViewController.swift
@@ -9,7 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //make isFinishedTypingNumber private so it can only be accessed inside this class
+    // a tracker to keep trakcing typing number has been done
+    // when typing is down, display the new single bit number
+    // when typing is still going, cancaternate the single bit number to previous number.
+    private var isFinishedTypingNumber: Bool = true
+    
+    
+    var displayNumber : String = ""
 
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    
+    @IBAction func calcButtonTapped(_ sender: UIButton) {
+        isFinishedTypingNumber = true
+    }
+    
+    @IBAction func numButtonTapped(_ sender: UIButton) {
+
+        guard let number = sender.currentTitle else {
+            print(" number button doesn't have text")
+            return
+        }
+        
+        // when finishing typing number by tapping calc buttons,show the next number to label.
+        if isFinishedTypingNumber {
+            displayLabel.text = number
+            isFinishedTypingNumber = false
+        } else {
+            // still typing the new number.
+//            displayNumber += number
+//            displayLabel.text = displayNumber
+            displayLabel.text = displayLabel.text! + number
+        }
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
